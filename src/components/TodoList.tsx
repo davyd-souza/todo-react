c3f68f8de11f9cc42ffb415d577eefca42d042a6
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2022-09-01 18:31:18 -0300

    refactor: implement todo list to manage each todo iteam

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,36 @@
+// COMPONENT
+import { TodoItem } from './TodoItem'
+
+// UTIL
+import { TodoProps } from "../interfaces"
+
+type TodoListProps = {
+	todos: TodoProps[]
+	completeAll: (todos: TodoProps[]) => void
+	toggleTodo: (id: string) => void
+}
+
+export function TodoList({ todos, completeAll, toggleTodo }: TodoListProps) {
+
+	const handleCompleteAll = () => completeAll(todos)
+
+	return (
+		<section>
+			<ul>
+				{
+					todos.map( ({id, title, completed, label}) => 
+						<TodoItem 
+							key={id} 
+							id={id}
+							title={title}
+							completed={completed}
+							label={label}
+							toggleTodo={toggleTodo}
+						/>
+					)
+				}
+			</ul>
+			<button onClick={handleCompleteAll}>Mark all complete</button>
+		</section>
+	)
+}
